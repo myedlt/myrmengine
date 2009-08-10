769ecd3d5514cfb1aa3dbcecb16557b3b008a513
huhongjun <huhongjun@gmail.com>
2009-08-10 14:42:17 +0000

    我的部署配置和license crack

--- RMEngine/PHP/ckfinder/config.php
+++ RMEngine/PHP/ckfinder/config.php
@@ -29,7 +29,7 @@ function CheckAuthentication()
 	//... where $_SESSION['IsAuthorized'] is set to "true" as soon as the
 	//user logs in your system.
 
-	return false;
+	return true;
 }
 
 // LicenseKey : Paste your license key here. If left blank, CKFinder will be
